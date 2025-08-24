a04b8c776ae9349b614d167334dc5ff9c7db5cab
Peeters-Jef <peeters.jef01@gmail.com>
2025-08-25 01:32:44 +0200

    what tis

--- aliases/general.aliases.sh
+++ aliases/general.aliases.sh
@@ -72,3 +72,4 @@ alias fix_stty='stty sane'                  # fix_stty:     Restore terminal set
 alias fix_term='echo -e "\033c"'            # fix_term:     Reset the conosle.  Similar to the reset command
 alias cic='bind "set completion-ignore-case on"' # cic:          Make tab-completion case-insensitive
 alias src='source ~/.bashrc'                # src:          Reload .bashrc file
+alias vim='nvim'
